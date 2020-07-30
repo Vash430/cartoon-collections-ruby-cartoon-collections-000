08b2a72ae69c3a4a3ea4414199d081975af71162
essential-form-0157 <vasmith430@gmail.com>
2020-07-30 13:53:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,26 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves) # code an argument here
+  dwarves.each_with_index do |dwarves, index|
+    index +=1
+    puts "#{index}.#{dwarves}" # Your code here
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)# code an argument here
+  planeteer_calls.collect do |call|
+    call.capitalize << "!" # Your code here
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(word)# code an argument here
+  word.any? do |word|
+    word.length > 4 # Your code here
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(cheese)# code an argument here
+   # the array below is here to help
+    cheese_types = ["cheddar", "gouda", "camembert"]
+    cheese.find do |cheese_types|
+    cheese_types.include?("cheddar")
+  end
 end
